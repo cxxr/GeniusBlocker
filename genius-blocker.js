4c74832488d059ee0bee7fc97c4f0337e6b58f9a
Sean Cassidy <sean.a.cassidy@gmail.com>
2016-04-19 18:42:42 -0700

    No need to block the script tag

--- genius-blocker.js
+++ genius-blocker.js
@@ -4,20 +4,9 @@ var geniusBlockerObserver = new MutationObserver(function(mutations) {
             for (var i = 0; i < mutation.addedNodes.length; i++) {
                 var item = mutation.addedNodes[i];
                 if (item.tagName !== undefined) {
-                    if (item.tagName === "SCRIPT") {
-                        var src = item.getAttribute("src");
-                        if (src !== undefined && (src.startsWith("http://genius.com/") || src.startsWith("https://genius.com/"))) {
-                            console.log("Removing ", item.tagName, src);
-                            document.body.removeChild(item);
-                        } else {
-                            console.log("Not removing ", item.tagName, src);
-                        }
-                    } else if (item.tagName.startsWith("GENIUS")) {
-                        console.log("Removing ", item.tagName);
+                    if (item.tagName.startsWith("GENIUS")) {
                         document.body.removeChild(item);
-                    } else {
-                        console.log("Not removing ", item.tagName);
-                    }
+                    } 
                 }
             }
         }
